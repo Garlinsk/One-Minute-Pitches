946d5eba860ff97ffd31accbf804fb2b5aef9eef
franklin <franklinwanami@gmail.com>
2021-08-19 08:08:22 +0300

    add templates to hold the html files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 
 # create an instace of my app
 app = Flask(__name__)
@@ -6,10 +6,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+     return render_template('index.html')
 
 
-     return "Lets build Pitches"
+@app.route('/login') 
+def login():
+    return render_template('login.html')
 
+
+@app.route('/singup') 
+def signup():
+    return render_template('signup.html')
+
+@app.route('/my_schedule') 
+def my_schedule():
+    return render_template('my_schedule.html') 
 
 if __name__ == '__main__':
     app.run(debug=True)
